64e48eca6af980daa35e75f6f3b8676b0acb0ab7
EKengineer <alikardik@gmail.com>
2022-09-03 22:13:40 +0300

    Создание репозиторя для хранения продуктов

---
+++ OnlainStore/Infrastructure/Store,Memory/ProductRepository.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Store_Memory
+{
+    public class ProductRepository : IProductRepository
+    {
+
+    }
+}
---
+++ OnlainStore/domain/Store/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Store
+{
+    public class Product
+    {
+        public int Id { get; }
+        public string Name { get; }
+        public decimal Cost { get; }
+        public string Description { get; }
+
+        public Product (int id, string name, decimal cost, string description)
+        {
+            Id = id;
+            Name = name;
+            Cost = cost;
+            Description = description;
+        }
+    }
+}
