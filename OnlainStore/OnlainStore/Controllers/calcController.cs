5d66c1692aa2e92b1479d133eea7d531f81935bb
EKengineer <alikardik@gmail.com>
2022-09-03 21:06:18 +0300

    Решение четвертого задания

--- OnlainStore/OnlainStore/Controllers/calcController.cs
+++ OnlainStore/OnlainStore/Controllers/calcController.cs
@@ -18,6 +18,7 @@ namespace OnlainStore.Controllers
                 case '/': return $"{a} / {b} = {a / b}";
                 case '-': return $"{a} - {b} = {a - b}";
                 default: return $"{a} + {b} = {a + b}";
+
             }
         }
 
